eeb7e1272665a549d1c95e210c4db6293f91925d
lkjhgtlopikl <d3tomaso@yandex.com>
2024-10-19 22:53:14 +0300

    изменение кнопок

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 
-import React, { useState, useEffect  } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
-import Button from './components/Button'; 
+import Button from './components/Button';
 import Code from './Code';
 
 function App() {
   const [activeComponent, setActiveComponent] = useState(null);
   const [codeStart, setCodeStart] = useState('');
 
-  
+
   const handleSendCodeClick = (e) => {
     console.log(e.currentTarget);
     setActiveComponent('Code');
@@ -17,7 +17,7 @@ function App() {
     if (newCodeStart) {
       setCodeStart(newCodeStart);
       console.log(newCodeStart);
-       // Это должно показать новое значение
+      // Это должно показать новое значение
     } else {
       console.error("data-value не найдено");
     }
@@ -29,7 +29,7 @@ function App() {
         <body>
           <Header>Выберите услугу</Header>
           <div className='buttons'>
-            <Button data-value={'АК'} onClick={handleSendCodeClick}>Отправить письмо</Button>
+            <Button  data-value={'АК'} onClick={handleSendCodeClick}>Отправить письмо</Button>
             <Button data-value={'БК'} onClick={handleSendCodeClick}>Отправить бандероль</Button>
             <Button data-value={'ВК'} onClick={handleSendCodeClick}>Отправить посылку</Button>
             <Button data-value={'ГК'} onClick={handleSendCodeClick}>Получить письмо/бандероль</Button>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import { Children } from "react";
 
-import React, { useState, useEffect  } from 'react';
-export default function Header({ children,onClick,value}) {
+import React, { useState, useEffect } from 'react';
+export default function Header({ children, onClick, value }) {
     const [id, setId] = useState(value);
     return (
-        
-          <button data-value = {value} onClick={onClick}  >{children}</button>
-        
-      );    
+
+        <button data-value={value} onClick={onClick}  >{children}</button>
+
+    );
 }
